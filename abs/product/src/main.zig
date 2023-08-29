7c7f2666d10eca8aee84449490889a579e03f6f2
mnj <mnj.rzr@gmail.com>
2023-08-29 21:13:06 +0900

    AtCoder Beginners Selection - Product

---
+++ abs/product/src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const abclib = @import("abclib");
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn();
+    const a_string = try abclib.readStringFromStdinUntilDelimiter(stdin, ' ');
+    const a = try abclib.stringToInt(a_string);
+    const b_string = try abclib.readStringFromStdinUntilDelimiter(stdin, '\n');
+    const b = try abclib.stringToInt(b_string);
+
+    if (@mod(a * b, 2) == 0) {
+        std.debug.print("Even", .{});
+    } else {
+        std.debug.print("Odd", .{});
+    }
+}
+
+test "simple test" {}
