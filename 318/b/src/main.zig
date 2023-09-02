6ac5c0ef42175bd0207b7869f57ced8b5e4fd1d6
mnj <mnj.rzr@gmail.com>
2023-09-02 23:21:34 +0900

    AtCoder Beginners Contest 318 - B: complete

--- 318/b/src/main.zig
+++ 318/b/src/main.zig
@@ -1,18 +1,31 @@
 const std = @import("std");
 const abclib = @import("abclib");
 
+const Type = u8;
+
 pub fn main() !void {
     const stdin = std.io.getStdIn();
-    const n = try abclib.stringToInt(try abclib.readStringFromStdinUntilDelimiter(stdin, '\n'), u8);
+    const n = try abclib.stringToInt(try abclib.readStringFromStdinUntilDelimiter(stdin, '\n'), Type);
+    var buf: [100][100]Type = std.mem.zeroes([100][100]Type);
+    var total: u16 = 0;
     for (abclib.range(n)) |_| {
         const a = try getInt(stdin, ' ');
         const b = try getInt(stdin, ' ');
         const c = try getInt(stdin, ' ');
         const d = try getInt(stdin, '\n');
-        std.debug.print("{d} {d} {d} {d}", .{a, b, c, d});
+        for (a..b) |i| {
+            for (c..d) |j| {
+                if (buf[i][j] == 1) {
+                    continue;
+                }
+                buf[i][j] = 1;
+                total += 1;
+            }
+        }
     }
+    std.debug.print("{d}", .{total});
 }
 
-fn getInt(stdin: std.fs.File, delimiter: u8) !i8 {
-    return try abclib.stringToInt(try abclib.readStringFromStdinUntilDelimiter(stdin, delimiter), i8);
-} 
+fn getInt(stdin: std.fs.File, delimiter: u8) !Type {
+    return try abclib.stringToInt(try abclib.readStringFromStdinUntilDelimiter(stdin, delimiter), Type);
+}
